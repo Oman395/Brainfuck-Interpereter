ea274c0cb0c33cca5eb9f4c8c29bc56f4445033c
Orander Robinson-Hartley <oranroha@gmail.com>
2021-12-02 22:58:27 -0500

    init

---
+++ index.js
@@ -0,0 +1,95 @@
+import fs from 'fs';
+const stdin = process.stdin;
+
+function prompt(question) {
+    return new Promise((resolve) => {
+        stdin.resume();
+        stdin.setEncoding('utf8');
+        process.stdout.write(question);
+        stdin.once('data', data => {
+            resolve(data.trim());
+        });
+    })
+};
+
+(async () => {
+    let file = await prompt('Enter file name: ');
+    let args = await prompt('Enter arguments, seperated by a comma: ')
+    args = args.split(',');
+    if (fs.existsSync(file)) {
+        let data = fs.readFileSync(file, 'utf8');
+        let split = data.toString().match(/[<>,\.\[\]\+-]/g);
+        let parsed = [];
+        let depth = 0;
+        split.forEach((char, index) => {
+            parsed[index] = {command: char, depth: depth};
+            switch(char) {
+                case '[':
+                    depth++;
+                    break;
+                case ']':
+                    depth--;
+                    let open;
+                    for (let i = index; i >= 0; i--) {
+                        if (parsed[i].command === '[' && parsed[i].depth === depth) {
+                            open = i;
+                            parsed[i].endLoc = index;
+                            break;
+                        }
+                    }
+                    parsed[index].origin = open;
+                    break;
+            }
+            if(index == split.length - 1) {
+                parsed[index].end = true;
+            }
+        });
+        let mem = [];
+        let pointer = 0;
+        let cmdPointer = 0;
+        let argsPointer = 0;
+        let running = true;
+        while (running) {
+            let cmd = parsed[cmdPointer];
+            if(!mem[pointer]) mem[pointer] = 0;
+            switch(cmd.command) {
+                case '<':
+                    pointer--;
+                    break;
+                case '>':
+                    pointer++;
+                    break;
+                case '+':
+                    mem[pointer]++;
+                    break;
+                case '-':
+                    mem[pointer]--;
+                    break;
+                case '.':
+                    console.log(mem[pointer]);
+                    break;
+                case ',':
+                    mem[pointer] = args[argsPointer] || args[args.length - 1];
+                    argsPointer++;
+                    break;
+                case '[':
+                    if (mem[pointer] === 0) {
+                        cmdPointer = cmd.endLoc;
+                    }
+                    break;
+                case ']':
+                    if (mem[pointer] !== 0) {
+                        cmdPointer = cmd.origin;
+                    }
+                    break;
+            }
+            if (parsed[cmdPointer].end) {
+                running = false;
+            }
+            cmdPointer++;
+        }
+    } else {
+        console.log('File does not exist');
+        process.exit(1);
+    }
+})();
