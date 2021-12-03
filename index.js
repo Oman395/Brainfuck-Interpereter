61a07054a178a5f6db41448d34d7c7c3046445f6
Orander Robinson-Hartley <oranroha@gmail.com>
2021-12-03 14:15:34 -0500

    comments/docs

--- index.js
+++ index.js
@@ -14,14 +14,29 @@ function prompt(question) {
 
 (async () => {
     let file = await prompt('Enter file name: ');
-    let args = await prompt('Enter arguments, seperated by a comma: ')
-    args = args.split(',');
     if (fs.existsSync(file)) {
+        let args = await prompt('Enter arguments, seperated by a comma: ')
+        args = args.split(',');
         let data = fs.readFileSync(file, 'utf8');
         let split = data.toString().match(/[<>,\.\[\]\+-]|{[a-zA-Z0-9]*}/g);
         let parsed = [];
         let depth = 0;
         let labels = {};
+        let pointTest = 0;
+        // This bad ~~code~~ boy is the lexer. It sorts through the commands, and gives them metadata so that I
+        // can work with them better.
+        // It keeps track of depth (scope) and where the pointer would be during execution
+        // The depth is incremented on '[' and decremented on ']'
+        // The pointer is incremented on '>', decremented on '<'
+        // And it can be set by labels
+        // When it gets to a label, it checks whether we have it's target stored.
+        // If it does, pointer goes there.
+        // If it doesn't, we save its position as that label's target.
+        // When we get to a closing bracket, we need to get it's start pos, as well as setting the start pos'
+        // closing bracket.
+        // We simple go backwords through the array, and check whether it is open brackets and on our level.
+        // It it is, we set the open value to it's location, and set it's close value to our location.
+        // That's basically it, brainfuck is simple as hell.
         split.forEach((char, index) => {
             parsed[index] = { command: char, depth: depth };
             switch (char[0]) {
@@ -44,6 +59,18 @@ function prompt(question) {
                     // Label
                     let label = char.replaceAll(/[{}]/g, '');
                     parsed[index].label = label;
+                    if (labels[label] || labels[label] == 0) {
+                        parsed[index].moveCount = labels[label] - pointTest;
+                        pointTest = labels[label];
+                    } else {
+                        labels[label] = pointTest;
+                    }
+                    break;
+                case '>':
+                    pointTest++;
+                    break;
+                case '<':
+                    pointTest--;
                     break;
             }
             if (index == split.length - 1) {
@@ -54,45 +81,48 @@ function prompt(question) {
         let pointer = 0;
         let cmdPointer = 0;
         let argsPointer = 0;
-        let running = true;
+        let running = true; 
+        // This is the main loop for the entire thing. It does whatever the command makes it do.
         while (running) {
             let cmd = parsed[cmdPointer];
             if (!mem[pointer]) mem[pointer] = 0;
             switch (cmd.command[0]) {
-                case '<':
+                case '<': // Dec pointer
                     pointer--;
                     break;
-                case '>':
+                case '>': // Inc pointer
                     pointer++;
                     break;
-                case '+':
+                case '+': // Inc mem at pointer
                     mem[pointer]++;
                     break;
-                case '-':
+                case '-': // Dec mem at pointer
                     mem[pointer]--;
                     break;
-                case '.':
+                case '.': // Output mem at pointer
                     console.log(mem[pointer]);
                     break;
-                case ',':
+                case ',': // This one needs a bit more explanation. It gets the current argument that is open,
+                // and sets the mem at pointer to it. It then increments the arg pointer, so we can get diff 
+                // args for the others.
+                // I'd just wait for user input, but brainfuck only accepts input at the start (as defined), so
+                // Here we are. No real difference honestly, but eh, i want it to be faithful.
                     mem[pointer] = args[argsPointer] || args[args.length - 1];
                     argsPointer++;
                     break;
-                case '[':
+                case '[': // If we are <=0 when we get here, it shouldnt loop, so go to end.
                     if (mem[pointer] <= 0) {
                         cmdPointer = cmd.endLoc;
                     }
                     break;
-                case ']':
+                case ']': // Return to open if not <= 0
                     if (mem[pointer] > 0) {
                         cmdPointer = cmd.origin;
                     }
                     break;
-                case '{':
-                    if (labels[cmd.label] || labels[cmd.label] == 0) {
-                        pointer = labels[cmd.label];
-                    } else {
-                        labels[cmd.label] = pointer;
+                case '{': // if it has a move count, move by it (cause some are just going to be declarations)
+                    if (cmd.moveCount) {
+                        pointer += cmd.moveCount;
                     }
                     break;
             }
--- parse.js
+++ parse.js
@@ -15,56 +15,83 @@ function prompt(question) {
 (async () => {
     let file = await prompt('Input file: ');
     if (!fs.existsSync(file)) throw new Error('File does not exist!');
-    let data = fs.readFileSync(file).toString().match(/[<>]|{[A-Za-z0-9]*}/g);
-    let pointer = 0;
+    let data = fs.readFileSync(file);
+    let split = data.toString().match(/[<>,\.\[\]\+-]|{[a-zA-Z0-9]*}/g);
+    let parsed = [];
+    let depth = 0;
     let labels = {};
-    data.forEach((item, index) => {
-        switch (item[0]) {
-            case '>':
-                pointer++;
-                data[index] = ''
+    let pointTest = 0;
+    /*
+    * This is the same lexer as in index.js, so if you want to know how it works, go there
+    */
+    split.forEach((char, index) => {
+        parsed[index] = { command: char, depth: depth };
+        switch (char[0]) {
+            case '[':
+                depth++;
                 break;
-            case '<':
-                data[index] = ''
-                pointer++
+            case ']':
+                depth--;
+                let open;
+                for (let i = index; i >= 0; i--) {
+                    if (parsed[i].command === '[' && parsed[i].depth === depth) {
+                        open = i;
+                        parsed[i].endLoc = index;
+                        break;
+                    }
+                }
+                parsed[index].origin = open;
                 break;
             case '{':
-                let label = item.replaceAll(/[{}]/g, '');
+                // Label
+                let label = char.replaceAll(/[{}]/g, '');
+                parsed[index].label = label;
                 if (labels[label] || labels[label] == 0) {
-                    let delta = pointer - labels[label];
-                    pointer = labels[label];
-                    let r = ''
-                    if (delta >= 1) {
-                        for (let i = 0; i < delta; i++) {
-                            r += '<'
-                        }
-                        data[index] = r;
-                    } else {
-                        for (let i = 0; i > delta; i--) {
-                            r += '>'
-                        }
-                        data[index] = r;
-                    }
+                    parsed[index].moveCount = labels[label] - pointTest;
+                    pointTest = labels[label];
                 } else {
-                    labels[label] = pointer;
-                    data[index] = '';
+                    labels[label] = pointTest;
                 }
+                break;
+            case '>':
+                pointTest++;
+                break;
+            case '<':
+                pointTest--;
+                break;
+        }
+        if (index == split.length - 1) {
+            parsed[index].end = true;
         }
     });
-    let d2 = fs.readFileSync(file).toString().match(/[<>,\.\[\]\+-]|{[a-zA-Z0-9]*}/g);
-    let currentLabel = 0;
-    let labelsDone = [];
-    data = data.filter(item => item != '');
-    d2.forEach((item, index) => {
-        if (item[0] == '{' && labelsDone.includes(item)) {
-            d2[index] = data[currentLabel];
-            currentLabel++;
-        } else if (item[0] == '{') {
-            labelsDone.push(item);
-            d2[index] = '';
+    // Convert backwards lol
+    // This method has one major issue: in bf, when I want to define a bunch of labels, i need to do like
+    // >{}>{}
+    // But then if I run it through my code, it keeps the >
+    // so it would parse to >><<
+    // oh well, too much effort to fix lol
+    // This function literally just goes through the commands, and replaces each label with it's assigned movecount
+    // of arrows (i.e. -3 would be <<<, 3 would be >>>)
+    let raw = ''
+    parsed.forEach(command => {
+        if(command.command[0] != '{') {
+            raw += command.command;
+        } else if(command.moveCount) {
+            let arrow = '';
+            if(command.moveCount > 0) { // Positive means it needs to be a >, negetive means <, and im too lazy to do
+                // absolute value then just like arrow += Math.sign(command.moveCount) == 1 ? '>' : '<';
+                // mostly b/c i just thought of it and its too much effort for me to implement
+                for(let i = 0; i < command.moveCount; i++) {
+                    arrow += '>';
+                }
+            } else {
+                for(let i = 0; i > command.moveCount; i--) {
+                    arrow += '<';
+                }
+            }
+            raw += arrow;
         }
     });
-    let merged = d2.join('');
-    fs.writeFileSync(`parsed${file}`, merged);
-    process.exit();
+    fs.writeFileSync(`parsed${file}`, raw);
+    process.exit(); // Too lazy to stop stdin, this works so eh
 })();
